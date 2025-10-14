384eca3c8a58e5453277d6334ee41405dec18299
Sneh Doshi <doshisneh75@gmail.com>
2025-10-14 22:13:03 +0530

    Added Firebase User Authentication Functionality

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMAAGMtg8TCErDikLYd_5S2ZZGMbHkBnY",
@@ -29,6 +30,24 @@ const signup = async (name , email , password) => {
     });
     
   } catch (error) {
+    console.log(error);
+    toast.error(error.code.split('/')[1].split('-').join(" "))
     
   }
 }
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth , email , password); 
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split('/')[1].split('-').join(" "))
+
+
+    
+  }
+}
+const logout = () => {
+  signOut(auth);
+}
+
+export {auth , db , signup , login , logout};
